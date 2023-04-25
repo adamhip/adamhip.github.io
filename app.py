b287c6c2f15c50e32f081ad1c95d5d7b25c598bd
Adam Hadi Pratama <87849245+adamhadip@users.noreply.github.com>
2023-04-25 21:23:28 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, render_template
+import pickle
+import pandas as pd
+import numpy as np
+import joblib
+scaler = joblib.load("A:/website/scalers.save")
+
+
+app = Flask(__name__, template_folder='template')
+model = joblib.load('A:/website/modelss.pkl')
+
+@app.route("/home")
+@app.route("/")
+def hello():
+    return render_template("home.html")
+
+@app.route("/predict", methods = ["GET", "POST"])
+def predict():
+    if request.method == "POST":
+        input_features = [float(x) for x in request.form.values()]
+        features_value = [np.array(input_features)]
+
+        feature_names = ["ph", "Hardness" , "Solids", "Chloramines", "Sulfate",
+                         "Conductivity", "Organic_carbon","Trihalomethanes", "Turbidity"]
+
+        df = pd.DataFrame(features_value, columns = feature_names)
+        df = scaler.transform(df)
+        output = model.predict(df)
+
+        if output[0] == 1:           
+            prediction = "aman"
+        else:
+            prediction = "tidak aman"
+
+
+        return render_template('home.html', prediction_text= "Air ini {} untuk manusia konsumsi ".format(prediction))
+
+        
+
+if __name__ == "__main__":
+    app.run(debug=True)
